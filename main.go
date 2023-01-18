52bac8c6d727d11bf7758d07c7c49eeb9c53d19d
Tarosul <tarusolyt@gmail.com>
2023-01-19 00:06:16 +0100

    Added Patch Path to toggle the Customer Completed Boolean. Customers can now get by the ID

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	_ "net/http"
@@ -22,6 +23,40 @@ func getCustomers(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, customers)
 }
 
+func toggleCustomerId(context *gin.Context) {
+	id := context.Param("id")
+	customer, err := getCustomerById(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Customer not Found"})
+	}
+
+	customer.Completed = !customer.Completed
+
+	context.IndentedJSON(http.StatusOK, customer)
+
+}
+
+func getCustomerById(id string) (*customer, error) {
+	for i, t := range customers {
+		if t.ID == id {
+			return &customers[i], nil
+		}
+	}
+	return nil, errors.New("Customer Not Found")
+}
+
+func getCustomer(context *gin.Context) {
+	id := context.Param("id")
+	customer, err := getCustomerById(id)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Customer not Found"})
+	}
+
+	context.IndentedJSON(http.StatusOK, customer)
+}
+
 func addCustomer(context *gin.Context) {
 	var newCustomer customer
 	if err := context.BindJSON(&newCustomer); err != nil {
@@ -37,6 +72,8 @@ func addCustomer(context *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/customers", getCustomers)
+	router.GET("/customers/:id", getCustomer)
+	router.PATCH("/customers/:id", toggleCustomerId)
 	router.POST("/addcustomer", addCustomer)
 	router.Run("localhost:9090")
 }
